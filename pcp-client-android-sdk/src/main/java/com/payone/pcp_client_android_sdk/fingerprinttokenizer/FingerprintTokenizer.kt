e16e4d80a79eb42311deca35a8820633a736c344
Nikolic Djordje <djordje.nikolic@rise-world.com>
2024-09-16 10:39:31 +0200

    implement FingerprintTokenizer

--- pcp-client-android-sdk/src/main/java/com/payone/pcp_client_android_sdk/fingerprinttokenizer/FingerprintTokenizer.kt
+++ pcp-client-android-sdk/src/main/java/com/payone/pcp_client_android_sdk/fingerprinttokenizer/FingerprintTokenizer.kt
@@ -1,10 +1,19 @@
-package com.payone.pcp_client_android_sdk.fingerprint_tokenizer
+package com.payone.pcp_client_android_sdk.fingerprinttokenizer
+
+/*
+ * This file is part of the PCPClient Android SDK.
+ * Copyright © 2024 PAYONE GmbH. All rights reserved.
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
 
 import android.content.Context
-import android.webkit.WebResourceError
+import android.util.Log
 import android.webkit.WebResourceRequest
 import android.webkit.WebView
 import android.webkit.WebViewClient
+import com.payone.pcp_client_android_sdk.utils.PCPEnvironment
 import java.util.UUID
 
 class FingerprintTokenizer(
@@ -14,30 +23,29 @@ class FingerprintTokenizer(
     private val environment: PCPEnvironment,
     sessionId: String? = null
 ) {
-
     private val snippetToken: String
     private var webView: WebView? = null
     private var onCompletion: ((Result<String>) -> Unit)? = null
+
     init {
         val uniqueId = sessionId ?: UUID.randomUUID().toString()
-        snippetToken = "${paylaPartnerId}_${partnerMerchantId}_${uniqueId}"
+        snippetToken = "${paylaPartnerId}_${partnerMerchantId}_$uniqueId"
     }
 
     fun getSnippetToken(onCompletion: (Result<String>) -> Unit) {
         this.onCompletion = onCompletion
         val script = makeScript()
         this.webView = makeInjectedWebView(script)
-        webView?.loadDataWithBaseURL(null, makeHTML(), "text/html", "UTF-8", null)
+        webView?.loadData(makeHTML(), "text/html", "UTF-8")
     }
-
 
     private fun makeHTML(): String {
         return """
             <!doctype html>
             <html lang="en">
-              <body></body>
+            <body></body>
             </html>
-        """.trimIndent()
+        """
     }
 
     private fun makeScript(): String {
@@ -49,45 +57,44 @@ class FingerprintTokenizer(
             script.src = 'https://d.payla.io/dcs/$paylaPartnerId/$partnerMerchantId/dcs.js';
             script.onload = function() {
                 if (typeof window.paylaDcs !== 'undefined' && window.paylaDcs.init) {
-                    window.paylaDcs.init('${environment.environment}', '$snippetToken');
-                }
-                else {
+                    window.paylaDcs.init('${environment.fingerprintTokenizerIdentifier}', '$snippetToken');
+                } else {
                     throw new Error('paylaDcs is not defined or does not have an init method.');
                 }
             };
             document.body.appendChild(script);
-        """.trimIndent()
+        """
     }
 
     private fun makeInjectedWebView(script: String): WebView {
         val webView = WebView(context)
         webView.settings.javaScriptEnabled = true
+        webView.settings.domStorageEnabled = true
 
         webView.webViewClient = object : WebViewClient() {
             override fun onPageFinished(view: WebView?, url: String?) {
                 view?.evaluateJavascript(script) { result ->
-                    if (result.contains("Error")) {
-                        onCompletion?.invoke(Result.failure(FingerprintError.Undefined))
-                    } else {
+                    try {
+                        Log.i("Fingerprint", "Successfully loaded snippet token.")
                         onCompletion?.invoke(Result.success(snippetToken))
+                    } catch (e: Exception) {
+                        Log.e(
+                            "Fingerprint",
+                            "Fingerprinting script failed with error: ${e.message}"
+                        )
+                        onCompletion?.invoke(Result.failure(FingerprintError.ScriptError(Throwable(e.message.toString()))))
                     }
                 }
             }
 
-            override fun onReceivedError(
+            override fun shouldOverrideUrlLoading(
                 view: WebView?,
-                request: WebResourceRequest?,
-                error: WebResourceError?
-            ) {
-                onCompletion?.invoke(Result.failure(FingerprintError.ScriptError(Throwable(error?.description.toString()))))
+                request: WebResourceRequest?
+            ): Boolean {
+                return false
             }
         }
+
         return webView
     }
-
-}
-
-sealed class FingerprintError : Throwable() {
-    data class ScriptError(val error: Throwable) : FingerprintError()
-    object Undefined : FingerprintError()
 }
