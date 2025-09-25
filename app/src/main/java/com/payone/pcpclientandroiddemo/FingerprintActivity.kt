44899c375d3eec2e0e4ba40edc222992068df25a
Blackfaded <r.heinen@nanogiants.de>
2025-09-25 14:47:37 +0200

    refactor: move fingerprint to activity

--- app/src/main/java/com/payone/pcpclientandroiddemo/FingerprintActivity.kt
+++ app/src/main/java/com/payone/pcpclientandroiddemo/FingerprintActivity.kt
@@ -4,17 +4,34 @@ import android.os.Bundle
 import android.widget.Button
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
-import com.payone.pcpclientandroiddemo.fingerprint.FingerprintTokenizerHandler
+import com.payone.pcp_client_android_sdk.fingerprinttokenizer.FingerprintTokenizer
+import com.payone.pcp_client_android_sdk.utils.PCPEnvironment
 
 class FingerprintActivity : AppCompatActivity() {
+    private lateinit var fingerprintTokenizer: FingerprintTokenizer
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_fingerprint)
         val tvSnippetToken = findViewById<TextView>(R.id.tvSnippetToken)
-        val fingerprintHandler = FingerprintTokenizerHandler(this, tvSnippetToken)
         val btnStart = findViewById<Button>(R.id.btnStartFingerprint)
+        fingerprintTokenizer = FingerprintTokenizer(
+            context = this,
+            paylaPartnerId = "e7yeryF2of8X",
+            partnerMerchantId = "test-1",
+            environment = PCPEnvironment.Test
+        )
         btnStart.setOnClickListener {
-            fingerprintHandler.startTokenization()
+            fingerprintTokenizer.getSnippetToken {
+                if (it.isSuccess) {
+                    val token = it.getOrNull() ?: ""
+                    android.util.Log.d("Success Token", token)
+                    tvSnippetToken.text = token
+                } else {
+                    val exception = it.exceptionOrNull()
+                    android.util.Log.d("Failure Token", exception?.message ?: "unknown")
+                    tvSnippetToken.text = exception?.message
+                }
+            }
         }
     }
 }
--- app/src/main/java/com/payone/pcpclientandroiddemo/MainActivity.kt
+++ app/src/main/java/com/payone/pcpclientandroiddemo/MainActivity.kt
@@ -1,27 +1,11 @@
 package com.payone.pcpclientandroiddemo
 
-import android.app.Activity
+
 import android.content.Intent
 import android.os.Bundle
-import android.util.Log
 import android.widget.Button
-import android.widget.TextView
-import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.ui.platform.ComposeView
-import com.google.android.gms.wallet.AutoResolveHelper
-import com.google.android.gms.wallet.PaymentsClient
-import com.google.android.gms.wallet.Wallet
-import com.google.android.gms.wallet.WalletConstants
-import com.google.android.gms.wallet.PaymentDataRequest
-import com.payone.pcpclientandroiddemo.fingerprint.FingerprintTokenizerHandler
-import com.payone.pcpclientandroiddemo.cctokenizer.CCTokenizerHandler
-import com.paypal.android.paymentbuttons.PayPalButton
-import com.payone.pcpclientandroiddemo.gpay.GooglePayHandler
-import com.payone.pcpclientandroiddemo.gpay.GooglePayRequestJson
-import com.payone.pcpclientandroiddemo.paypal.PayPalHandler
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
